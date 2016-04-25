02020642dd5983b19d38f4bff128f61c33b047c2
Changshuo Gao <cgao@healthrecoverysolutions.com>
2016-04-25 12:02:58 -0400

    working version

--- README.md
+++ README.md
@@ -2,3 +2,17 @@ Before run this script please manually check several things
 1. check the crontab (type crontab -e)
 2. check the place where store the sscripts( default under /root/backups/)
 3. check the log file(default under /var/log/duplicity/etc.log)
+
+Secondly, the -t, --time, and --restore-time options take a time string, which can be given in any of several formats:
+
+1.
+the string "now" (refers to the current time)
+2.
+a sequences of digits, like "123456890" (indicating the time in seconds after the epoch)
+3.
+A string like "2002-01-25T07:00:00+02:00" in datetime format
+4.
+An interval, which is a number followed by one of the characters s, m, h, D, W, M, or Y (indicating seconds, minutes, hours, days, weeks, months, or years respectively), or a series of such pairs. In this case the string refers to the time that preceded the current time by the length of the interval. For instance, "1h78m" indicates the time that was one hour and 78 minutes ago. The calendar here is unsophisticated: a month is always 30 days, a year is always 365 days, and a day is always 86400 seconds.
+5.
+A date format of the form YYYY/MM/DD, YYYY-MM-DD, MM/DD/YYYY, or MM-DD-YYYY, which indicates midnight on the day in question, relative to the current time zone settings. For instance, "2002/3/5", "03-05-2002", and "2002-3-05" all mean March 5th, 2002.
+
--- build.sh
+++ build.sh
@@ -18,12 +18,15 @@ KEY="Unknown"
 
 #config variables
 CONFIG_FILE_NUM=11
-CONFIG_DB_NUM=15
+CONFIG_DB_NUM=16
 #cronjobs
 CRON_JOBS=()
 
 #scripts directory
 SCRIPT_DIR=~/backups/
+
+#mysql dump main directory
+#DUMP_DIR=~/mysqldump/
 
 #temp crontab file
 TEMP_CRONTAB=mycron
@@ -42,7 +45,7 @@ function writeIntoFile(){
 	#create directory
 	mkdir -p $SCRIPT_DIR
 	#naming script file based on timestamp
-	desdir=${SCRIPT_DIR}$(date +%Y-%m-%d_%H:%M:%S:%N).sh
+	desdir=${SCRIPT_DIR}$(date +%Y-%m-%d-%H-%M-%S-%N).sh
 	#echo "$desdir"
 	touch ${desdir}
 	if [ -f "$desdir" ]; then
@@ -83,7 +86,60 @@ function loadCrontab(){
 }
 
 
+function validateBackupType(){
+	type="$1"
+	if [[  "$ype" == i* ]] || [[ "$type" == I* ]]; then
+		echo "incremental";
+	elif [[ "$type" == f* ]] || [[ "$type" == F* ]]; then		
+		echo "full";	
+	elif [ "$type" == '*' ]; then
+		echo "";
+	else
+		echo "ERROR";
+	fi
+}
 
+function validateBackupSave(){
+	b_save="$1"
+	re='^[0-9]+$'
+        if [[ "$b_save" =~ $re ]]; then
+        	echo "true"
+
+       	elif [ "$b_save" = '*' ]; then
+              	echo "false"
+                #echo "$has_backup_save";
+        else
+       		echo "ERROR"
+        fi
+}
+
+function validateTimeSave(){
+	t_save="$1"
+	if [ "$t_save" = '*' ]; then
+		echo "false"
+	else
+		echo "true"
+	fi
+
+}
+
+function setDBDumpDirectory(){
+	host="$1"
+	DUMP_DIR="$2"
+	spec_dir="${DUMP_DIR}/${host}/"
+	mkdir -p $spec_dir
+	#return variable
+	echo "$spec_dir"
+}
+
+function setDBDumpFile(){
+	host_dir="$1"
+	des_file=${host_dir}$(date +%Y-%m-%d-%H-%M-%S-%N).sql
+	touch ${des_file}
+	#return variable
+	echo ${des_file}
+
+}
 
 # Ensure we are root
 if [ ! "`whoami`" = "root" ]; then
@@ -166,13 +222,13 @@ esac
 done
 
 
-if [ $FILE_MODE == 1 ] && [ $DATABASE_MODE == 0 ]; then
+if [ "$FILE_MODE" = 1 ] && [ "$DATABASE_MODE" = 0 ]; then
         #Ensure we have config file
         if [ ! -f $CONFIG_FILE ]; then
                 echo "Error: config_file file not found!";
                 exit 1;
         fi
-elif [ $FILE_MODE == 0 ] && [ $DATABASE_MODE == 1 ]; then
+elif [ "$FILE_MODE" = 0 ] && [ "$DATABASE_MODE" = 1 ]; then
         #Ensure we have config file
         if [ ! -f $CONFIG_DB ]; then
                 echo "Error: config_db file not found!";
@@ -216,7 +272,7 @@ done < $ENCRYPTION_FILE
 
 copyCrontab
 
-if [ $FILE_MODE==1 ] && [ $DATABASE_MODE==0 ]; then 
+if [ "$FILE_MODE" = 1 ] && [ "$DATABASE_MODE" = 0 ]; then 
 	#read config file
 	i=0;
 	while read LINE
@@ -249,43 +305,24 @@ if [ $FILE_MODE==1 ] && [ $DATABASE_MODE==0 ]; then
 				backup_save=${CONFIG_ARRAY[9]};
 				time_save=${CONFIG_ARRAY[10]};
 				#validate the backup_type
-				if [[  "$backup_type" == i* ]] || [[ "$backup_type" == I* ]]; then
-					backup_type="incremental";
-				elif [[ "$backup_type" == f* ]] || [[ "$backup_type" == F* ]]; then		
-					backup_type="full";	
-				elif [ "$backup_type" == '*' ]; then
-					backup_type="";
-				else
-					echo "Error: config file invalid!";
-					exit 1;	
-				fi
+                                backup_type=$(validateBackupType "$backup_type")
+                                if [ "$backup_type" = "ERROR" ]; then
+                                        echo "Error: config file invalid!"
+                                        exit 1
+                                fi
 				
-				has_backup_save=false
-				has_time_save=false
-				#validate the backup_save
-				re='^[0-9]+$'
-				if [[ "$backup_save" =~ $re ]]; then
-					has_backup_save=true;
-					#echo "$has_backup_save";
-
-				elif [ "$backup_save" = '*' ]; then
-				
-					has_backup_save=false;
-					#echo "$has_backup_save";
-				else
-					echo "Error: config file invalid!";
-					exit 1;
-				fi
-				
-				#validate the time_save
-				
-				if [ "$time_save" = '*' ]; then
-					has_time_save=false;
-				
-				else
-					has_time_save=true;
-				fi
-				
+				#backup_save
+                                has_backup_save=$(validateBackupSave "$backup_save")
+                                if [ "$has_backup_save" = "ERROR" ]; then
+                                        echo "ERROR: dbconfig file invalid!"
+                                        exit 1
+                                fi
+                                #time_save
+                                has_time_save=$(validateTimeSave "$time_save")
+                                if [ "$has_time_save" = "ERROR" ]; then
+                                        echo "ERROR: dbconfig file invalid!"
+                                        exit 1
+                                fi
 				
 				#fomat script
 				schedule="${m} ${h} ${dom} ${mon} ${dow}";
@@ -323,7 +360,8 @@ if [ $FILE_MODE==1 ] && [ $DATABASE_MODE==0 ]; then
 		fi
 	done < $CONFIG_FILE
 
-elif [ $FILE_MODE==0 ] && [ $DATABASE_MODE==1 ]; then
+#database version
+elif [ "$FILE_MODE" = 0 ] && [ "$DATABASE_MODE" == 1 ]; then
 	#read config file
 	j=0;
 	while read LINE
@@ -362,7 +400,108 @@ elif [ $FILE_MODE==0 ] && [ $DATABASE_MODE==1 ]; then
 				backup_type=${DB_ARRAY[12]};
 				backup_save=${DB_ARRAY[13]};
 				time_save=${DB_ARRAY[14]};
-				#TODO	
+				dump_save=${DB_ARRAY[15]};
+				#TODO:need to finish database
+				#validate these variables
+				#db_host
+				if [ "$db_host" = '*' ]; then
+					db_host="localhost"
+				fi
+				#db user name
+				if [ "$user_name" = '*' ]; then 
+					echo "Error: invalid database username!"
+					exit 1
+				fi
+				#db password
+				if [ "$password" = '*' ]; then
+					password=""
+				fi
+				#backup_type
+				backup_type=$(validateBackupType "$backup_type")
+				if [ "$backup_type" = "ERROR" ]; then
+					echo "Error: db config file invalid!"
+					exit 1
+				fi
+				#backup_save
+				has_backup_save=$(validateBackupSave "$backup_save")
+				if [ "$has_backup_save" = "ERROR" ]; then
+					echo "ERROR: dbconfig file invalid!"
+					exit 1
+				fi
+				#time_save
+				has_time_save=$(validateTimeSave "$time_save")
+				if [ "$has_time_save" = "ERROR" ]; then
+					echo "ERROR: dbconfig file invalid!"
+					exit 1
+				fi
+				#dump_save
+				has_dump_save=$(validateBackupSave "$dump_save")
+				if [ "$has_dump_save" = "ERROR" ]; then
+					echo "ERROR: dbconfig file invalid!"		
+					exit 1
+				fi
+				#validate mysql connection
+				echo "validate mysql connection..."
+				if [ "$dbname" = '*' ]; then
+					if ! mysql -u$user_name -p$password -h$db_host -e ";" ; then
+						echo "Error: cannot connect to mysql!"
+						exit 1
+					else
+						echo "success!"
+					fi 
+				else
+					if ! mysql -u$user_name -p$password -h$db_host -e "use ${dbname}" ; then
+						echo "Error: cannot connect to mysql!"
+						exit 1
+					else 
+						echo "success!"
+					fi
+				fi
+				#create or use local folder
+				mkdir -p ${local_folder}
+				#format script
+				schedule="${m} ${h} ${dom} ${mon} ${dow}";
+				head="#${schedule}\n#!/bin/bash";
+				
+				#db dump part
+				#set up directory and file
+				dump_dir=$(setDBDumpDirectory "$db_host" "$local_folder")
+				#dump_file=$(setDBDumpFile "$dump_dir")				
+				#dump_file="des_file=${dump_dir}'$(date +%Y-%m-%d-%H-%M-%S-%N).sql'\n touch '$des_file'"				
+				dump_file_name='$(date +%Y-%m-%d-%H-%M-%S-%N).sql'
+				dump_file_variable='$des_file'
+				dump_file="des_file=${dump_dir}${dump_file_name}\ntouch ${dump_file_variable}"				
+
+				dump=""
+				if [ "$dbname" = '*' ]; then
+					dump="$(which mysqldump) -u${user_name} -p${password} -h${db_host} --all-databases > ${dump_file_variable}"
+				else
+					dump="$(which mysqldump) -u${user_name} -p${password} -h${db_host} ${dbname} > ${dump_file_variable}"
+				fi
+
+				#remove previous files(based on days)
+				if [ "$has_dump_save" = true ]; then 
+					remove_old_dumps="$(which find) ${dump_dir}* -mmin +${dump_save} -type f -delete"
+					dump=$remove_old_dumps' \n'$dump
+				fi
+				#duplicity part
+				dup="export PASSPHRASE=\"${PASSPHRASE}\" \n$(which duplicity) ${backup_type} --encrypt-key ${KEY} ${local_folder} sftp://${backup_host}/${backup_folder} >> ${LOG_DIR}${LOG_FILE}";
+				if [ "$has_backup_save" = true ]; then
+					b="$(which duplicity) remove-all-but-n-full ${backup_save} --force sftp://${backup_host}/${backup_folder} >> ${LOG_DIR}${LOG_FILE}";
+					dup=$dup'\n'$b;
+				fi
+
+
+				if [ "$has_time_save" = true ]; then
+					b="$(which duplicity) remove-older-than ${time_save} --force sftp://${backup_host}/${backup_folder} >> ${LOG_DIR}${LOG_FILE}";
+					dup=$dup'\n'$b;
+					#echo -e "$dup";
+				fi
+				context=$head'\n'$dump_file'\n'$dump'\n'$dup;
+				#echo -e "$context"
+				dir=$(writeIntoFile "$context")
+				addIntoCrontab "$dir" "$schedule" "$TEMP_CRONTAB"
+				j=$((j+1));
 			else
                                 echo "Error: db config file invalid!"
                                 exit 1
@@ -384,7 +523,7 @@ loadCrontab
 #add these cronjobs to the crontab
 #for j in "${CRON_JOBS[@]}"
 #do
-#	echo "${j}" >> mycron
+#echo "${j}" >> mycron
 #done
 #install crontab
 #crontab mycron
